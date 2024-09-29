fee0266da9ee55e04ca1a7714190aabb216ffed2
Kaique-GM <kaiquegmoficial@gmail.com>
2024-09-29 17:12:46 -0300

    Inserção de usuário com POST

--- mongo/mongo/src/main/java/com/springmongo/mongo/services/UserService.java
+++ mongo/mongo/src/main/java/com/springmongo/mongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.springmongo.mongo.domain.User;
+import com.springmongo.mongo.dto.UserDTO;
 import com.springmongo.mongo.repository.UserRepository;
 import com.springmongo.mongo.services.Exception.ObjectNotFoundException;
 
@@ -20,8 +21,16 @@ public class UserService {
         return repo.findAll();
     }
 
-    public User findById(String id){
-       Optional<User> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDTO) {
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
 }
