1d336394be28fae12e3c5a15fbf7035284265f2e
Kaique-GM <kaiquegmoficial@gmail.com>
2024-09-30 11:11:06 -0300

    Acrescentando comentários aos posts

--- mongo/mongo/src/main/java/com/springmongo/mongo/config/Instantiation.java
+++ mongo/mongo/src/main/java/com/springmongo/mongo/config/Instantiation.java
@@ -7,11 +7,11 @@ import java.util.TimeZone;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.core.aggregation.DateOperators.Timezone;
 
 import com.springmongo.mongo.domain.Post;
 import com.springmongo.mongo.domain.User;
 import com.springmongo.mongo.dto.AuthorDTO;
+import com.springmongo.mongo.dto.CommentDTO;
 import com.springmongo.mongo.repository.PostRepository;
 import com.springmongo.mongo.repository.UserRepository;
 
@@ -43,10 +43,18 @@ public class Instantiation implements CommandLineRunner {
                 new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje", new AuthorDTO(maria));
 
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!",sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite",sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um otimo dia!",sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1,c2));
+        post2.getComments().addAll(Arrays.asList(c3));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
         
         maria.getPosts().addAll((Arrays.asList(post1,post2)));
         userRepository.save(maria);
+
     }
 
 }
